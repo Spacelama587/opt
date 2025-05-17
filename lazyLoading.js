38f61dd640b9fd577025c563b44fe7ac11502d7e
thevijeeth@gmail.com <thevijeeth@gmail.com>
2025-05-17 08:54:48 +0530

    no lzy loading

--- lazyLoading.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  setTimeout(function() {
-    const heroArea = document.querySelector('.hero-area');
-    
-
-    heroArea.style.backgroundImage = 'url("./images/hero-bg.min.webp")';
-  }, 200);
-});
